8e5e215380cc82e8dcd9b8bc5b01cb823348ff83
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2024-02-13 16:48:19 +0100

    service layer of Job

--- src/main/java/it/johnson/firstjobapp/job/JobController.java
+++ src/main/java/it/johnson/firstjobapp/job/JobController.java
@@ -1,27 +1,34 @@
 package it.johnson.firstjobapp.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class JobController {
 
-    private List<Job> jobs = new ArrayList<>();
+    private JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
 
     @GetMapping("/jobs")
     public List<Job> findAll(){
-        return jobs;
+        return jobService.findAll();
     }
 
     @PostMapping("/jobs")
     public String createJob(@RequestBody Job job){
-        jobs.add(job);
+        jobService.createJob(job);
         return "Job added successfully";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id){
+        Job job = jobService.getJobById(id);
+        if(job != null)
+            return job;
+        return new Job(1L, "Dummy Job", "You are Jobless", "Free", "Free", "Home");
     }
 }
---
+++ src/main/java/it/johnson/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,37 @@
+package it.johnson.firstjobapp.job.impl;
+
+import it.johnson.firstjobapp.job.Job;
+import it.johnson.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for(Job job: jobs){
+            if(job.getId().equals(id)){
+                return job;
+            }
+        }
+        return null;
+    }
+
+
+}
